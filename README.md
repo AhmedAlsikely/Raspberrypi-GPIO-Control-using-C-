ac997a45924074b2c54477c5ad44fdc0fd1c8057
AhmedAlsikely <ahmed.alsikely773@gmail.com>
2024-01-31 12:27:04 +0200

    New Update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Raspberrypi-GPIO-Control-using-Cpp
+
+g++ main.cpp classes/Led/led.cpp -o binaryfile
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "classes/Led/led.hpp"
+
+int main() {
+
+    LED led_20("20");
+    led_20.Led_Blink(10, 1);
+
+    return 0;
+}
